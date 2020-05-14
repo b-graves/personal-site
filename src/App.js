ff0c675b1e9db06af5794f886286867fcdbb86dd
Ben Graves <btg628@cs.bham.ac.uk>
2020-05-14 17:38:41 +0100

    https redirect

--- src/App.js
+++ src/App.js
@@ -2,24 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HttpsRedirect from 'react-https-redirect';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <HttpsRedirect>
+      <div className="App">
+        <header className="App-header">
+
+          <p>
+            Ben Graves
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </HttpsRedirect>
   );
 }
 
