d88e2511cb7ec6b08be73ec0efb9dcc897eebb74
Ben Graves <56936483+b-graves@users.noreply.github.com>
2023-10-23 13:36:56 +0100

    Update edition.js

--- src/components/SmoothieMakerPage/edition.js
+++ src/components/SmoothieMakerPage/edition.js
@@ -394,7 +394,7 @@ export const prints = [
     {
         number: 50,
         fileName: "50.jpg",
-        isAvailable: true,
+        isAvailable: false,
         isMisprint: false,
         isTshirt: false,
         size: null
